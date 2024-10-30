515048cac091de5d789add489cb3483a1e668916
saturnaliam <llcsaturnaliam@gmail.com>
2024-10-30 17:17:19 -0500

    changed to default with navigation

---
+++ app/_layout.tsx
@@ -0,0 +1,14 @@
+import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import 'react-native-reanimated';
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ title: "Index" }} />
+    </Stack>
+  );
+}
