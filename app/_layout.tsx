886d957acf051225ef61a2bcc5ca4190fc170d7a
PramitaUp <pramita0919@gmail.com>
2024-11-04 16:22:59 -0600

    Counting points for teleop
    * created a function to count points
    
    * fixed style issues

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -9,6 +9,7 @@ export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ title: "Index" }} />
+      <Stack.Screen name="teleop" options={{ title: "Teleop" }} />
     </Stack>
   );
 }
