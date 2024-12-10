ff07070edb949c02a7ce5922097738f0e6948e01
PramitaUp <pramita0919@gmail.com>
2024-12-10 00:24:39 +0000

    Changed auto points input method

--- app/auto.tsx
+++ app/auto.tsx
@@ -1,10 +1,38 @@
 import Checkbox from "./Components/Checkbox";
 import NumberInputBox from "./Components/NumberInputBox";
+import { Button, Text, View } from "react-native";
+import { useState } from "react";
 
 
 function Auto() {
+
+  const [count, setCount] = useState(0);
+    
+    if (count < 0) {
+        setCount(count + 1);
+    }
+    
+    if (count >8){
+      setCount(count-1);
+    }
+
+    const addCount = (a: number) => {
+        setCount(count + a);
+    }
+
   return (
-    <NumberInputBox label="Notes scored during auto" min={0} max={8}/>
+    <>
+  <Button 
+    title="+"
+    onPress={() => addCount(1)} />
+
+ <Button
+    title="-"
+    onPress={() => addCount(-1)} />
+
+  <Text>{count}</Text>
+
+    </>
   )
 }
 
